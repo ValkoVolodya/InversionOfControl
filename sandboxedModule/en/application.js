36ec40bfd7508305567816303de501c78fcb0893
Valko Vova <valko.volodya@gmail.com>
2016-03-09 19:07:28 +0200

    Completed Tasks 1, 2, 7, 8

--- sandboxedModule/en/application.js
+++ sandboxedModule/en/application.js
@@ -5,7 +5,29 @@
 // Print from the global context of application module
 console.log('From application global context');
 
-module.exports = function() {
+var isNumber = util.isNumber;
+console.log("Is Infinity a number?\n" + isNumber(Infinity));
+
+var intervalID = setInterval(function(){
+    console.log("On the edge of tomorrow...");
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(intervalID);
+    console.log("So now the cycle is end.");
+}, 5000);
+
+console.log(this.module.toString());
+
+module.exports = {}
+
+module.exports.doSomething = function() {
 	// Print from the exported function context
   console.log('From application exported function');
 };
+
+module.exports.variableName = 'stringVariable';
+
+module.exports.sum = function(a, b) {
+  console.log(a + " + " + b + " = " + (a + b));
+}
--- sandboxedModule/en/framework.js
+++ sandboxedModule/en/framework.js
@@ -4,11 +4,20 @@
 
 // The framework can require core libraries
 var fs = require('fs'),
-    vm = require('vm');
+    vm = require('vm'),
+    util = require('util');
 
 // Create a hash and turn it into the sandboxed context which will be
 // the global context of an application
-var context = { module: {}, console: console };
+var context = {
+    module: {},
+    console: console,
+    setInterval: setInterval,
+    setTimeout: setTimeout,
+    clearInterval: clearInterval,
+    util: util
+};
+
 context.global = context;
 var sandbox = vm.createContext(context);
 
@@ -16,11 +25,17 @@ var sandbox = vm.createContext(context);
 var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
   // We need to handle errors here
-  
+
   // Run an application in sandboxed context
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
-  
+  var s = sandbox.module.exports;
+  s.doSomething();
+  console.log(s.variableName);
+  s.sum(2, 4);
+  var str = s.sum.toString();
+  console.log(str);
+  console.log(str.slice(str.indexOf('(') + 1, str.indexOf(')')));
   // We can access a link to exported interface from sandbox.module.exports
   // to execute, save to the cache, print to console, etc.
 });
